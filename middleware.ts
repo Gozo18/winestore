605ac57dbf6cb6714a5c8b18f114ac07a99a9f36
Roman Jelinek <jelinek82@gmail.com>
2025-11-21 13:35:42 +0100

    Middleware size redused

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,21 @@
-export { auth as middleware } from "@/auth"
+/* export { auth as middleware } from "@/auth" */
+
+import { NextResponse } from "next/server"
+import { getToken } from "next-auth/jwt"
+
+export async function middleware(req: Request) {
+  const token = await getToken({
+    req,
+    secret: process.env.AUTH_SECRET,
+  })
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/unauthorized", req.url))
+  }
+
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: ["/admin/:path*"],
+}
