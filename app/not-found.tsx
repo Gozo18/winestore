1dac8d1e0fa3f9b2a452850dcff0c6f071943aaf
Roman Jelinek <jelinek82@gmail.com>
2026-01-07 13:29:59 +0100

    Not found page mobile view + typo fix in carousel

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -7,13 +7,14 @@ const NotFoundPage = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <Image
-        src="/images/logo.svg"
-        width={48}
-        height={48}
+        src="/images/logo_only.png"
+        width={100}
+        height={100}
         alt={`${APP_NAME} logo`}
         priority={true}
+        className="mb-8"
       />
-      <div className="p-6 w-1/3 rounded-lg shadow-md text-center">
+      <div className="p-6 w-3/4 md:w-1/3 rounded-lg shadow-md text-center">
         <h1 className="text-3xl font-bold mb-4">Nenalezeno...</h1>
         <p className="text-destructive">
           Stránka, kterou hledáte, nebyla nalezena.
